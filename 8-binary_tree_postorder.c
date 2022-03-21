f3625962eefca1b8251fa7b8dea35ef3590d3f49
camia97 <3962@holbertonschool.com>
2022-03-21 12:59:54 -0700

    add 7 and 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - function post-order traversal
+ * @tree: pointer to the root
+ * @func: pointer to the func
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);	
+	func(tree->n);
+}
