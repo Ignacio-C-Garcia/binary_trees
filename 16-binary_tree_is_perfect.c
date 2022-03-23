28d752a095e9b786fce3737c087a6e68dbc48c44
camia97 <3962@holbertonschool.com>
2022-03-23 10:32:46 -0700

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -45,23 +45,32 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (height_l - height_r);
 }
 /**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to tree
+ * Return: true or false
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	return (0);
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to node
  * Return: true or false
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int isperfect = 0;
-
 	if (!tree)
 		return (0);
-
-	isperfect = binary_tree_balance(tree);
-	if (isperfect != 0)
-		return (0);
-	if (tree->left)
-		binary_tree_is_perfect(tree->left);
-	if (tree->right)
-		binary_tree_is_perfect(tree->right);
-	return (1);
+	if (binary_tree_is_full(tree) == 1)
+		if (binary_tree_balance(tree->left) == 0 &&
+				binary_tree_balance(tree->right) == 0)
+			return (1);
+	return (0);
 }
