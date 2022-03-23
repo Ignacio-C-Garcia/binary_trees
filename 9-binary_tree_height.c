1f087b4eb7eb6e9a325dbf7266d962d7e32e096e
camia97 <3962@holbertonschool.com>
2022-03-23 06:55:50 -0700

    betty 9  and 8

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,7 +13,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		height_l = 1 + binary_tree_height(tree->left);
 	else
 		height_l = 0;
-	
+
 	if (tree->right)
 		height_r = 1 + binary_tree_height(tree->right);
 	else
