c383204b5c69903248f6366503963b8595fd11a8
camia97 <3962@holbertonschool.com>
2022-03-23 07:30:33 -0700

    changes 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,26 +1,22 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - function that goes through a binary tree
+ * binary_tree_size - function that measures the size
  * @tree: pointer to root
- * @func: pointer to function to call each node
+ * Return: the size or null
  */
-size_t recursive_counter(const binary_tree_t *tree, size_t counter)
-{
-        if (tree->left)
-	{
-                counter = 1 + recursive_counter(tree->left, counter);
-	}
-        if (tree->right)
-	{
-                counter = 1 + recursive_counter(tree->right, counter);
-	}
-        return (counter);
-}
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        size_t counter = 1;
+	size_t counter = 1;
 
 	if (tree == NULL)
 		return (0);
-        return (recursive_counter(tree, counter));
+	if (tree->left)
+	{
+		counter += binary_tree_size(tree->left);
+	}
+	if (tree->right)
+	{
+		counter += binary_tree_size(tree->right);
+	}
+	return (counter);
 }
